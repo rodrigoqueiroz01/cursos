60288876b60baeb6269016f96f503c04e954415c
rodrigoqueiroz01 <rodrigoq1212@gmail.com>
2023-10-17 00:08:58 -0300

    iniciando curso

---
+++ api-restfull/rest_springboot/src/test/java/br/com/dev/rq/rest_springboot/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.rq.rest_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
