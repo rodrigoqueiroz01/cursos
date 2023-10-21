ebb3d0e5559cef3637798dc4789bcecc782e97f0
rodrigoqueiroz01 <rodrigoq1212@gmail.com>
2023-10-21 15:29:57 -0300

    add dependencias para integrar com o PostgreSQL

--- rest_springboot/src/main/java/br/com/dev/rq/rest_springboot/model/Person.java
+++ rest_springboot/src/main/java/br/com/dev/rq/rest_springboot/model/Person.java
@@ -1,6 +1,9 @@
 package br.com.dev.rq.rest_springboot.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serial;
 import java.io.Serializable;
