6f19e0e7a252e5042529fbe0c23d3ce4becad694
rodrigoqueiroz01 <rodrigoq1212@gmail.com>
2023-10-21 00:49:03 -0300

    tratamento de excessões

--- rest_springboot/src/main/java/br/com/dev/rq/rest_springboot/exception/handler/ApiError.java
+++ rest_springboot/src/main/java/br/com/dev/rq/rest_springboot/exception/handler/ApiError.java
@@ -1,7 +1,8 @@
 package br.com.dev.rq.rest_springboot.exception.handler;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.io.Serial;
@@ -9,7 +10,8 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Getter
-@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiError implements Serializable {
 
     @Serial
@@ -18,7 +20,9 @@ public class ApiError implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private final LocalDateTime timestamp = LocalDateTime.now();
 
-    private String message;
-    private String details;
+    private Integer status;
+    private String type;
+    private String title;
+    private String detail;
 
 }
