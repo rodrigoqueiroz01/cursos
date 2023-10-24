5c527ab66516cf77477a3acd7679da842de68011
rodrigoqueiroz01 <rodrigoq1212@gmail.com>
2023-10-23 21:04:16 -0300

    update

--- rest_springboot/src/main/java/br/com/dev/rq/rest_springboot/repository/PersonRepository.java
+++ rest_springboot/src/main/java/br/com/dev/rq/rest_springboot/repository/PersonRepository.java
@@ -2,7 +2,5 @@ package br.com.dev.rq.rest_springboot.repository;
 
 import br.com.dev.rq.rest_springboot.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> { }
