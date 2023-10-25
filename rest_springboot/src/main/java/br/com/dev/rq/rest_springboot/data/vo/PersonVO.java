79aa2c3e985f2f21bae333beb966fdfe45651b5b
rodrigoqueiroz01 <rodrigoq1212@gmail.com>
2023-10-25 00:04:19 -0300

    update

--- rest_springboot/src/main/java/br/com/dev/rq/rest_springboot/data/vo/PersonVO.java
+++ rest_springboot/src/main/java/br/com/dev/rq/rest_springboot/data/vo/PersonVO.java
@@ -1,7 +1,5 @@
 package br.com.dev.rq.rest_springboot.data.vo;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,28 +12,16 @@ import java.io.Serializable;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonPropertyOrder({"id", "address", "firstName", "lastName", "gender"})
 public class PersonVO implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
-    @JsonProperty("id")
     private Long id;
-
-    @JsonProperty("nome")
     private String firstName;
-
-    @JsonProperty("sobrenome")
     private String lastName;
-
-    @JsonProperty("cpf")
     private String document;
-
-    @JsonProperty("endereço")
     private String address;
-
-    @JsonProperty("genero")
     private String gender;
 
 }
