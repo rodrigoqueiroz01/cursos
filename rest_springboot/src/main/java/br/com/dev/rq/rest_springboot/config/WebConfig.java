b782974d26ee6a2b7caf547e6b2aaf10118521c8
rodrigoqueiroz01 <rodrigoq1212@gmail.com>
2023-11-01 09:26:28 -0300

    update

--- rest_springboot/src/main/java/br/com/dev/rq/rest_springboot/config/WebConfig.java
+++ rest_springboot/src/main/java/br/com/dev/rq/rest_springboot/config/WebConfig.java
@@ -1,9 +1,11 @@
 package br.com.dev.rq.rest_springboot.config;
 
 import br.com.dev.rq.rest_springboot.util.HttpMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -12,6 +14,9 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
 
     @Override
     public void extendMessageConverters(List<org.springframework.http.converter.HttpMessageConverter<?>> converters) {
@@ -26,6 +31,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .mediaType("json", MediaType.APPLICATION_JSON)
                 .mediaType("xml", MediaType.APPLICATION_XML)
                 .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                .allowedMethods("*") // * Permite para todos os métodos GET, POST, PUT, DELETE, etc
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true);
     }
 
 }
