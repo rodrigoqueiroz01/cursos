59ba722f26303c40b4eb08d1c13b915c741d7fe1
rodrigoqueiroz01 <rodrigoq1212@gmail.com>
2023-11-06 10:03:40 -0300

    update

--- typescript-alura/parte01/cap01/app/app.ts
+++
@@ -1,6 +0,0 @@
-import {Negociacao} from "./models/negociacao.js";
-
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao.data);
-console.log(negociacao.volume);
-console.log(negociacao.valor);
