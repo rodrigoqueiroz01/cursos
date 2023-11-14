9cb36b8b0700e83030ecd6a2026cec4b49c3609f
rodrigoqueiroz01 <rodrigoq1212@gmail.com>
2023-11-13 22:52:52 -0300

    update

--- angular14-crud/memoteca/src/app/app.module.ts
+++ angular14-crud/memoteca/src/app/app.module.ts
@@ -1,22 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
-import { RodapeComponent } from './components/rodape/rodape.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CabecalhoComponent} from './components/cabecalho/cabecalho.component';
+import {RodapeComponent} from './components/rodape/rodape.component';
+import {CriarPensamentoComponent} from './components/pensamentos/criar-pensamento/criar-pensamento.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponent,
-    RodapeComponent
+    RodapeComponent,
+    CriarPensamentoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
