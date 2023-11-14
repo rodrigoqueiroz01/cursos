bca411a08c00b012b387fff9122ee269a7339152
rodrigoqueiroz01 <rodrigoq1212@gmail.com>
2023-11-14 11:03:47 -0300

    update

---
+++ angular14-crud/memoteca/src/app/components/pensamentos/pensamento.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import * as http from "http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PensamentoService {
+
+  constructor(http: HttpClient) { }
+
+  public adicionar(): void {
+
+  }
+
+  public listar(): void {
+
+  }
+
+  public editar(): void {
+
+  }
+
+  public excluir(): void {
+
+  }
+
+}
