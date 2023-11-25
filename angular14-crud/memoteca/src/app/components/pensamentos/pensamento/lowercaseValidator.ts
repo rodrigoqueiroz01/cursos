f83852074e9b8bfbaafe045cd7d2e2f5057d95fe
rodrigoqueiroz01 <rodrigoq1212@gmail.com>
2023-11-25 13:34:44 -0300

    update

--- angular14-crud/memoteca/src/app/components/pensamentos/pensamento/lowercaseValidator.ts
+++
@@ -1,6 +0,0 @@
-import {AbstractControl} from "@angular/forms";
-
-export function lowercaseValidator(control: AbstractControl) {
-  const autoria = control.value as string;
-  return autoria !== autoria?.toLowerCase() ? {'lowerCase': true} : null;
-}
