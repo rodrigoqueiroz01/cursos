c455ac4da526e90f4b393a2b7792364533b663e8
rodrigoqueiroz01 <rodrigoq1212@gmail.com>
2023-12-06 11:00:14 -0300

    update

---
+++ angular-alura/forms-angular-main/src/app/service/consulta-cep.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConsultaCepService {
+
+  private urlAPI: string = 'https://viacep.com.br/ws/'
+
+  constructor(
+      private http: HttpClient
+  ) {}
+
+  public getConsultaCep(cep: string): Observable<Object> {
+    return this.http.get(`${this.urlAPI}${cep}/json`);
+  }
+
+}
