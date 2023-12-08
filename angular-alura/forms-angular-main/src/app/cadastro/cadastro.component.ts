01ab162d165818900bc719148e9bb5e29a535847
rodrigoqueiroz01 <rodrigoq1212@gmail.com>
2023-12-08 10:08:46 -0300

    update

--- angular-alura/forms-angular-main/src/app/cadastro/cadastro.component.ts
+++ angular-alura/forms-angular-main/src/app/cadastro/cadastro.component.ts
@@ -1,30 +1,47 @@
-import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from "@angular/forms";
-import { ConsultaCepService } from "../service/consulta-cep.service";
-import { Subscription } from "rxjs";
+import {Router} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {ConsultaCepService} from "../service/consulta-cep.service";
 
 @Component({
-  selector: 'app-cadastro',
-  templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+    selector: 'app-cadastro',
+    templateUrl: './cadastro.component.html',
+    styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent implements OnInit {
 
-  constructor(
-      private router: Router,
-      private consultaCepService: ConsultaCepService
-  ) {}
+    constructor(
+        private router: Router,
+        private consultaCepService: ConsultaCepService
+    ) {
+    }
 
-  ngOnInit(): void { }
+    ngOnInit(): void {
+    }
 
-  public cadastrar(form: NgForm): void {
-    form.valid ? this.router.navigate(['./sucesso']) : alert('Formulário inválido!');
-  }
+    public cadastrar(form: NgForm): void {
+        form.valid ? this.router.navigate(['./sucesso']) : alert('Formulário inválido!');
+    }
 
-  public consultaCEP(event: any): Subscription {
-    const cep = event.target.value;
-    return this.consultaCepService.getConsultaCep(cep).subscribe(resultado => console.log(resultado));
-  }
+    public consultaCEP(event: any, formulario: NgForm) {
+        const cep = event.target.value;
+
+        if (cep != '') {
+            this.consultaCepService.getConsultaCep(cep).subscribe(resultado => {
+                console.log(resultado);
+                this.populandoEndereco(resultado, formulario);
+            });
+        }
+    }
+
+    public populandoEndereco(dados: any, formulario: NgForm) {
+        formulario.form.patchValue({
+            endereco: dados.logradouro,
+            complemento: dados.complemento,
+            bairro: dados.bairro,
+            cidade: dados.localidade,
+            estado: dados.uf
+        })
+    }
 
 }
